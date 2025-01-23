76f1d462bc3313ca54df7eb26d43ae1dfac2949b
dfavatoe <d.favato@outlook.com>
2025-01-23 10:11:48 +0100

    Data Fetched, Product Cards Built

---
+++ src/components/Grid.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ProductT } from "../types/type";
+import { Col, Container, Row } from "react-bootstrap";
+import "./ProductCard.css";
+import ProductCard from "./ProductCard";
+
+type GridProps = {
+  products: ProductT[];
+};
+
+function Grid({ products }: GridProps) {
+  return (
+    <Container className="justify-content-md-center">
+      <Row className="g-1">
+        {products &&
+          products.map((product, index) => {
+            return (
+              <Col>
+                <ProductCard key={product.id} product={product} />
+              </Col>
+            );
+          })}
+      </Row>
+    </Container>
+  );
+}
+
+export default Grid;
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Card, Stack } from "react-bootstrap";
+import { ProductT } from "../types/type";
+import "./ProductCard.css";
+
+type ProductCardProps = {
+  product: ProductT;
+};
+
+function ProductCard({ product }: ProductCardProps) {
+  return (
+    <Card className="zoom" style={{ width: "18rem" }}>
+      <Card.Img className="image" variant="top" src={product.thumbnail} />
+      {/* <Card.Text>{product.description}</Card.Text> */}
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          {product.price} €
+        </Card.Subtitle>
+        <Stack gap={3}>
+          <Card.Link className="mb-2" href="#">
+            Learn more
+          </Card.Link>
+        </Stack>
+        <Button variant="warning">Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProductCard;
